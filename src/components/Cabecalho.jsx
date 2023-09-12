de35e6db5756fb6d1fda3da6e9d8ea251c897870
vcamargo04 <viniciusac2005@Hotmail.com>
2023-09-11 23:15:32 -0300

    Alinhando os botões no cabecalho.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -13,22 +13,15 @@ export default function Cabecalho() {
             <nav>
             <div className="btn-cabecalho-1">
 
-            <button className="btn-cabecalho"><Link className="link" to="/home">Home</Link></button>
+            
             <button className="btn-cabecalho"><Link className="link" to="/login">Login</Link></button>
             <button className="btn-cabecalho"><Link className="link" to="/cadastro">Cadastro</Link></button> 
-            
-            
-            </div>
-            <div className="btn-cabecalho-2">
             <button className="btn-cabecalho"><Link className="link" to="/descricao">Descrição</Link></button>
             <button className="btn-cabecalho"><Link className="link" to="/integrantes">Integrantes</Link></button>
             
             
-            
-            
-            
             </div>
-
+            
             </nav>
         </div>
         </header>
