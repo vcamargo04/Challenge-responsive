d5af988228f0fa32c633b476997721cdb7570246
gbalttazar <gbalttazar.sit@gmail.com>
2023-09-11 23:37:42 -0300

    ajustes finais

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -16,8 +16,8 @@ export default function Cabecalho() {
             
             <button className="btn-cabecalho"><Link className="link" to="/login">LOGIN</Link></button>
             <button className="btn-cabecalho"><Link className="link" to="/cadastro">CADASTRO</Link></button> 
-            <button className="btn-cabecalho"><Link className="link" to="/descricao">Descrição</Link></button>
-            <button className="btn-cabecalho"><Link className="link" to="/integrantes">Integrantes</Link></button>
+            <button className="btn-cabecalho"><Link className="link" to="/descricao">DESCRIÇÃO</Link></button>
+            <button className="btn-cabecalho"><Link className="link" to="/integrantes">INTEGRANTES</Link></button>
             
             
             </div>
